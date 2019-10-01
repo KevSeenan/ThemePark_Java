2b45ddd92fed2ccc8531ca2e36c04e14c0324ac0
KevSeenan <kev.seenan@gmail.com>
2019-10-01 17:16:38 +0100

    Adds default and price for under twelves for dodgems

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,28 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
 
-    public Dodgems(String name, int rating) {
+public class Dodgems extends Attraction implements ITicketed {
+
+    double price;
+
+    public Dodgems(String name, int rating, double price) {
         super(name, rating);
+        this.price = price;
+    }
+
+    @Override
+    public double defaultPrice() {
+        return this.price;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        double underTwelvePrice = 8.40;
+        if(visitor.getAge() >= 2.0) {
+            underTwelvePrice = defaultPrice() / 2;
+        }
+        return underTwelvePrice;
     }
 }
