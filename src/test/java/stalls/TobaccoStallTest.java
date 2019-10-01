5bd05f6d207f2520cf47417e9e4caaacebd62149
KevSeenan <kev.seenan@gmail.com>
2019-10-01 17:53:55 +0100

    Adds IReviewed to stalls

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -13,7 +13,7 @@ public class TobaccoStallTest {
 
     @Before
     public void setUp() throws Exception {
-        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 3);
         visitor = new Visitor(22,2.2,40);
     }
 
@@ -35,5 +35,10 @@ public class TobaccoStallTest {
     @Test
     public void canBuyTobacco() {
         assertEquals(true, tobaccoStall.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void canGetARating() {
+        assertEquals(3, tobaccoStall.getRating());
     }
 }
