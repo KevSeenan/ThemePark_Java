30fd562e0613ae9abcce7dca721b45e27013f9fa
KevSeenan <kev.seenan@gmail.com>
2019-10-01 21:35:45 +0100

    Adds ThemePark class

---
+++ src/main/java/themepark/ThemePark.java
@@ -0,0 +1,12 @@
+package themepark;
+
+public class ThemePark {
+
+    private String attraction;
+    private String stall;
+
+    public ThemePark(String attraction, String stall) {
+        this.attraction = attraction;
+        this.stall = stall;
+    }
+}
